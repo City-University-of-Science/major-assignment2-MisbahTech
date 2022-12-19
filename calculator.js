11b1a98bdcfc661bc1d5855f360452f5b6390f30
misbah112001jan@gmail.com <misbah112001jan@gmail.com>
2022-12-19 12:37:11 +0500

    12

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
 //select elements
 
 const input_element = document.querySelector('.input');
-const output_element = document.querySelector('.result value');
-const output_result_element = document.querySelector('.result value');
+const output_element = document.querySelector('.result.value');
+const output_result_element = document.querySelector('.result.value');
 
 
 //  SOME VARIABLES
@@ -14,6 +14,8 @@ let data = {
     operation : [],
     formula : []
 }
+let ans = 0;
+// CALCULATOR BUTTONS
 
 let calculator_buttons = [];
 
@@ -28,7 +30,7 @@ function createCalculatorButtons(){
         }
 
         const row = document.querySelector(".row:last-child");
-        row.innerHTML += '<button id=" ${button.name}">
+        row.innerHTML += '<button id="${button.name}">
                                    ${button.symbl}
                                    </button>';
        
@@ -39,3 +41,81 @@ function createCalculatorButtons(){
 
 createCalculatorButtons();
 
+// CLICK EVENT LISTENER
+input_element.addEventListener("click", event =>{
+    const target_btn = event.target;
+
+    calculator_buttons.foreach(button => {
+        if(button.name == target_btn.id) calculator (button);
+    })
+})
+
+
+//Calculator
+function calculator(button){
+    if(button.type == "operator"){
+     data.operation.push(button.symbol);
+     data.formula.push(button.formula);
+    }else if(button.type == "number"){
+        data.operation.push(button.symbol);
+        data.formula.push(button.formula);
+    }else if(button.type == "trigo_function"){
+
+    } else if(button.type == "math_function"){
+      console.log("math")
+      let symbol, formula;
+
+      if(button.name == "factorial"){
+        symbol ="|";
+        formula = button.formula;
+        data.operation.push(symbol);
+        data.formula.push(formula);
+      }else if(button.name == "power"){
+        symbol ="^(";
+        formula = button.formula;
+        data.operation.push(symbol);
+        data.formula.push(formula);
+      } else if (button.name == "square"){
+        symbol ="^(";
+        formula = button.formula;
+        data.operation.push(symbol);
+        data.formula.push(formula);
+        data.operation.push("2");
+        data.formula.push("2");
+      }{
+        symbol=button.symbol +"(";
+        formula = button.formula +"(";
+        data.operation.push(symbol);
+        data.formula.push(formula);
+      }
+    } else if(button.type =="key"){
+        if(button.type == "clear"){
+            data.operation =[];
+            data.formula = [];
+
+            updateOutputResult(0);
+        }
+
+    } else if(button.name == "delete"){
+        data.operation.pop();
+        data.formula.pop();
+    }
+    else if(button.type == "calculator"){
+        formula_str = data.formula.join('');
+
+        let result = eval(formula_str);
+
+        updateOutputResult(result);
+    }
+
+
+    UpdateOutputOperation(data.operation.join(''));
+}
+
+//update output
+function UpdateOutputOperation(operation){
+    output_operation_element.innerHTML== operation 
+}
+function UpdateOutputResult(result){
+    output_result_element.innerHTML = result
+}
